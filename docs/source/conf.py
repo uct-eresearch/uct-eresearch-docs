8cbf804dc3aeee05a3f1b5cb17c292620558e4ec
Anelda van der Walt <anelda.vdwalt@gmail.com>
2025-08-15 17:01:17 +0200

    remove lumache

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'UCT eResearch'
+copyright = '2025, University of Cape Town'
+author = 'UCT eResearch'
 
 release = '0.1'
 version = '0.1.0'
